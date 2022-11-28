83ec2bd223fd389a52e0e1582389731cafcb1380
gunnar.kistner@gmail.com <gunnar.kistner@gmail.com>
2022-11-28 16:53:59 +0100

    flexing

---
+++ services/frontend/src/components/Sidebar.svelte
@@ -0,0 +1,11 @@
+<div class="h-full shadow-md bg-white absolute" id="sidenav">
+    <ul class="relative px-1">
+        <li class="relative">
+            <span> 1st item </span>
+        </li>
+        <li class="relative">
+            <span> 2nd item </span>
+        </li>
+    </ul>
+
+</div>
--- services/frontend/src/routes/+page.svelte
+++ services/frontend/src/routes/+page.svelte
@@ -1,45 +1,49 @@
-<script>
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
+<script lang="ts">
+	import { T, Canvas, InteractiveObject, OrbitControls} from '@threlte/core'
 	import { spring } from 'svelte/motion'
 	import { degToRad } from 'three/src/math/MathUtils'
+	import Sidebar from "../components/Sidebar.svelte";
 
-	const scale = spring(1)
+	const scale = spring(2)
 </script>
 
-<div>
-  <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
-<div>
-	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
-		</T.PerspectiveCamera>
+<div class="h-screen">
+	<div class="text-center w-auto">
+		<h1 class="text-3xl font-bold underline ">Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</div>
+	<div class="flex h-full">
+		<Sidebar/>
+		<Canvas class="h-full basis-3/4">
+			<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+				<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+			</T.PerspectiveCamera>
 
-		<T.DirectionalLight castShadow position={[3, 10, 10]} />
-		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-		<T.AmbientLight intensity={0.2} />
+			<T.DirectionalLight castShadow position={[3, 10, 10]} />
+			<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+			<T.AmbientLight intensity={0.2} />
 
-		<!-- Cube -->
-		<T.Group scale={$scale}>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-				<!-- Add interaction -->
-				<InteractiveObject
-					object={ref}
-					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
-				/>
+			<!-- Cube -->
+			<T.Group scale={$scale}>
+				<T.Mesh position.y={0.5} castShadow let:ref>
+					<!-- Add interaction -->
+					<InteractiveObject
+						object={ref}
+						interactive
+						on:pointerenter={() => ($scale = 2)}
+						on:pointerleave={() => ($scale = 1)}
+					/>
 
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
+					<T.BoxGeometry />
+					<T.MeshStandardMaterial color="#333333" />
+				</T.Mesh>
+			</T.Group>
+
+			<!-- Floor -->
+			<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
+				<T.CircleGeometry args={[3, 72]} />
+				<T.MeshStandardMaterial color="white" />
 			</T.Mesh>
-		</T.Group>
-
-		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
-	</Canvas>
+		</Canvas>
+	</div>
 </div>
